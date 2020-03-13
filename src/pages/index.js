8fc02e64548e6261a0f70bd8968ba814a7d4dada
ea.dev3 <ea.dev3@gmail.com>
2020-03-13 14:38:15 -0700

    Particle Background

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,84 @@ import "../components/styles/style.css"
 
 import Footer from "../components/footer"
 import Header from "../components/header"
+
+import Particles from "react-particles-js"
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+}
+
 const IndexPage = () => (
   <div>
+    <Particles
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        zIndex: -1,
+      }}
+      params={{
+        particles: {
+          number: {
+            value: 80,
+            density: {
+              enable: false,
+            },
+          },
+          size: {
+            value: 3,
+            random: true,
+            anim: {
+              speed: 4,
+              size_min: 0.3,
+            },
+          },
+          line_linked: {
+            enable: false,
+          },
+          move: {
+            random: true,
+            speed: 1,
+            direction: "top-left",
+            out_mode: "out",
+          },
+        },
+        interactivity: {
+          events: {
+            onhover: {
+              enable: true,
+              mode: "bubble",
+            },
+            onclick: {
+              enable: true,
+              mode: "repulse",
+            },
+          },
+          modes: {
+            bubble: {
+              distance: 250,
+              duration: 2,
+              size: 0,
+              opacity: 0,
+            },
+            repulse: {
+              distance: 400,
+              duration: 4,
+            },
+          },
+        },
+      }}
+    />
     <Header />
     <div className="homepage">
       <SEO title="Home" />
@@ -27,17 +103,17 @@ const IndexPage = () => (
       <div className="btnrow">
         <AniLink cover to="contact">
           <button className="button">
-            <a className="btnlink" href="">
+            <a className="btnlink" href="#contact">
               Get In Touch
             </a>
           </button>
         </AniLink>
-        <button className="button">
+        <button className="buttonResume">
           <a
             className="btnlink"
-            href="https://drive.google.com/open?id=1G8zw95uXzs2PJjURlfpuAXNWiY66Gy_b"
+            href="https://drive.google.com/file/d/1zKQrwcB-O3D4e3ezgPsKs6IUD93Od4c1/view"
           >
-            &nbsp;Resume&nbsp;
+            &nbsp;My Resume&nbsp;
           </a>
         </button>
       </div>
